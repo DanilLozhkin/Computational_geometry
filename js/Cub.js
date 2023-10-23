43346148748dde7308f6b741ebbf461e197391b8
DanilLozhkin <117292325+DanilLozhkin@users.noreply.github.com>
2023-10-23 18:17:51 +0300

    итог

--- js/Cub.js
+++ js/Cub.js
@@ -1,9 +1,10 @@
-
 const canvas = document.getElementById("canvas");
 const engine = new BABYLON.Engine(canvas, true);
 
-const createScene = function () {
-  const scene = new BABYLON.Scene(engine);
+let scene;
+
+const createScene = async function () {
+  const newScene = new BABYLON.Scene(engine);
 
   const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.Vector3(0, 0, 0));
   camera.attachControl(canvas, true);
@@ -12,25 +13,28 @@ const createScene = function () {
   const groundMat = new BABYLON.StandardMaterial("groundMat");
   groundMat.diffuseColor = new BABYLON.Color3(0, 1, 0);
 
-  const result = BABYLON.SceneLoader.ImportMeshAsync("", "", "untitled.glb", scene);
+  const result = await BABYLON.SceneLoader.ImportMeshAsync("", "resurs/", "untitled.glb", newScene);
 
-  result.position = new BABYLON.Vector3(0, 0, 0);
-  result.scaling.x = 0.75;
   result.material = groundMat;
+  result.meshes[0].scaling = new BABYLON.Vector3(x.value, y.value, z.value);
+  //result.scaling = new BABYLON.Vector3(2, 1.5, 3);
 
-  return scene;
+  scene = newScene; // Присваиваем созданную сцену переменной scene
 };
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const scene = createScene();
 
-
-engine.runRenderLoop(function () {
-  scene.render();
-});
 
 window.addEventListener("resize", function () {
   engine.resize();
 });
+
+const form = document.getElementById("form");
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  createScene().then(() => {
+    engine.runRenderLoop(function () {
+      scene.render();
+    });
+  });
 });
